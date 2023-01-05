020de4be6ec0e36df0790996a60477e45e89c989
naiknaveen338 <naiknaveen338@gmail.com>
2023-01-06 00:43:33 +0530

    This is our Fourth commit But a error

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained from second commit")
+
+        This is second error
     }
 }

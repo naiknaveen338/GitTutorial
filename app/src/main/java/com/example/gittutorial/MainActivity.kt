ec8b5079a051458eba648d75674d1fd74ea36f2c
naiknaveen338 <naiknaveen338@gmail.com>
2023-01-06 00:40:12 +0530

    Revert "This is our Third commit from Android Studio But a error"
    This reverts commit 23dcccfe8f28b43eb088a36ca0d49a048a02285d.

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -9,7 +9,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained from second commit")
-
-        today is a good day which is eror
     }
 }

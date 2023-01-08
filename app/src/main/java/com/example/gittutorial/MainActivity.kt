32471b65467e036bb8d6f7a92aca308ea7c635cf
naiknaveen338 <naiknaveen338@gmail.com>
2023-01-08 23:36:12 +0530

    Change 2 in testing branch , to check merge conflict

--- app/src/main/java/com/example/gittutorial/MainActivity.kt
+++ app/src/main/java/com/example/gittutorial/MainActivity.kt
@@ -12,6 +12,6 @@ class MainActivity : AppCompatActivity() {
 
         println("Stash faile ")
 
-        println("Comnmit in Develoopment branch")
+        println("Change 2 in Testing branch for checking merge conflict")
     }
 }
